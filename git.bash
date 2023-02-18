62f02741473c5b33ec6a5b01377ff7335a83ea18
Ha1O7 <76957722+Ha1O7@users.noreply.github.com>
2023-02-18 04:37:53 +0000

    new_version

---
+++ git.bash
@@ -0,0 +1,4 @@
+cd /Users/ha1o/Desktop/Zihan
+git add .
+git commit -m 'new_version'
+git push origin master
--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+cd /Users/ha1o/Desktop/Zihan
 git add .
-git commit -m 'initial'
-git push
+git commit -m 'new_version'
+git push origin master
